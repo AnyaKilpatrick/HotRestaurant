2f42656da70fd0e772952759ee2f1e632b35ba2d
Anya <hannakilpatrick@gmail.com>
2018-04-24 19:25:26 -0400

    First Commit

---
+++ hotrestaurant.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT||3000;
+
+// Sets up the Express app to handle data parsing
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+var reserves = [
+    
+]
+
+
+app.get("/home", function(req, res){
+    res.sendFile(path.join(__dirname, "home.html"));
+})
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+
